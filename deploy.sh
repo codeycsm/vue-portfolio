07f35182bea4b91a6197cee3045aa6df0b6b4460
Codey McDaniel <codeymcdaniel@gmail.com>
2020-10-31 17:27:03 -0600

    changed publicPath

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
 # abort on errors
-
 set -e
 
 # build
@@ -10,17 +9,17 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain 
+# if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
 git init
-git add -a
+git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:codeycsm/codeycsm.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git.github.com:codeycsm/vue-portfolio.git master:gh-pages
+git push -f git@github.com:codeycsm/vue-portfolio.git master:gh-pages
 
 cd -
